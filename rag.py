e67c2d605d4da3a56f7e7439c8b59e892563c315
sbkaracan <sbkaracan@gmail.com>
2026-01-25 14:26:05 +0100

    log improvement

--- rag.py
+++ rag.py
@@ -112,10 +112,9 @@ class RAGSystem(BaseModel):
             logger.info(f"Loading PDF file from {self.file_path}")
             loader = PyPDFLoader(self.file_path)
             data = loader.load()
-            #logger.info(f"Loaded {len(data)} documents")
             return data
         except Exception as e:
-            print(f"Error loading data: {e}")
+            logger.error(f"Error loading data: {e}")
             return None
     
     def _split_data(self) -> list[Document]:
@@ -144,12 +143,12 @@ class RAGSystem(BaseModel):
             controlled by self.chunk_size and self.chunk_overlap attributes.
         """
         try:
-            logger.info(f"Splitting data into chunks")
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
             chunks = text_splitter.split_documents(self._load_data())
+            logger.info(f"Splitted data into chunks")
             return chunks
         except Exception as e:
-            print(f"Error splitting data: {e}")
+            logger.error(f"Error splitting data: {e}")
             return None
     
     def _embed_data_google(self) -> GoogleGenerativeAIEmbeddings:
@@ -180,7 +179,7 @@ class RAGSystem(BaseModel):
             embeddings = GoogleGenerativeAIEmbeddings(model=self.model_embedding)
             return embeddings
         except Exception as e:
-            print(f"Error embedding data: {e}")
+            logger.error(f"Error embedding data: {e}")
             return None
     
     def _store_data(self) -> Chroma:
@@ -210,24 +209,20 @@ class RAGSystem(BaseModel):
         """
         try:
             if os.path.exists(self.db_name):
-                logger.info(f"Loading existing Chroma database from {self.db_name}")
                 vectorstore = Chroma(
                     persist_directory=self.db_name,
                     embedding_function=self._embed_data_google()
                 )
-                #logger.info(f"Loaded existing Chroma database from {self.db_name}")
+                logger.info(f"Loaded existing Chroma database from {self.db_name}")
             else:
-                logger.info(f"Creating new Chroma database in {self.db_name}")
                 vectorstore = Chroma.from_documents(
                 documents=self._split_data(), 
                 embedding=self._embed_data_google(),
                 persist_directory=self.db_name)
-                #logger.info(f"Created new Chroma database in {self.db_name}")
-            
-            #vectorstore.persist()
+                logger.info(f"Created new Chroma database in {self.db_name}")
             return vectorstore
         except Exception as e:
-            print(f"Error storing data: {e}")
+            logger.error(f"Error storing data: {e}")
             return None
         
     
@@ -252,14 +247,14 @@ class RAGSystem(BaseModel):
             - self.search_kwargs: Additional parameters (e.g., {"k": 3} for top-3 results)
         """
         try:
-            logger.info(f"Retrieving data from Chroma")
             retriever = self._store_data().as_retriever(
                 search_type=self.search_type,
                 search_kwargs=self.search_kwargs
             )
+            logger.info(f"Retrieved data from Chroma")
             return retriever
         except Exception as e:
-            print(f"Error retrieving data: {e}")
+            logger.error(f"Error retrieving data: {e}")
             return None
     
     def _prompt_template(self) -> ChatPromptTemplate:
@@ -306,11 +301,10 @@ class RAGSystem(BaseModel):
             controlled by self.model_llm and self.temperature attributes.
         """
         try:
-            #logger.info(f"LLM")
             llm = ChatGoogleGenerativeAI(model=self.model_llm, temperature=self.temperature)
             return llm
         except Exception as e:
-            print(f"Error LLM: {e}")
+            logger.error(f"Error LLM: {e}")
             return None
     
     @staticmethod
@@ -370,16 +364,17 @@ class RAGSystem(BaseModel):
             - Output parser: Converts to string
         """
         try:
-            logger.info(f"Chain")
-            return (
+            chain = (
             {"context": RunnableLambda(lambda x: x["question"]) | self._retriever() | self.format_docs
             , "question": RunnablePassthrough()} 
             | self._prompt_template() 
             | self._llm_google() 
             | StrOutputParser()
             )
+            logger.info(f"Chain created")
+            return chain
         except Exception as e:
-            print(f"Error chain: {e}")
+            logger.error(f"Error chain: {e}")
             return None
 
     def ask(self, question: str) -> str:
@@ -415,6 +410,6 @@ class RAGSystem(BaseModel):
             logger.info(f"Question: {question}")
             logger.info(f"Answer: {result}")
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(f"Error: {e}")
             return None
         return result
